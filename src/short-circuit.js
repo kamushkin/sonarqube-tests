0d1c8a11b59702bc101875128e755996214b0567
kamushkin <kamushkin@users.noreply.github.com>
2017-01-30 16:06:23 +0100

    reproducing jQuery conditions

--- src/short-circuit.js
+++ src/short-circuit.js
@@ -5,11 +5,25 @@ function notBoolean(promise, doSomething){
 	// jQuery
 	//   https://sonarqube.com/issues#issues=AVl2B2tcoyrXF8JkMTQy
 
-	var then = promise && promise.then;
+	var then = promise &&
+		( typeof promise === "object" || typeof promise === "function" ) &&
+		promise.then;
 
 	if (then) {
 		then.call(doSomething)
 	}
 }
 
+
+function notBoolean2(value, doSomething){
+
+	// jQuery
+	//   https://sonarqube.com/issues#issues=AVl2B2tcoyrXF8JkMTQy
+
+	var then = value && value.then;
+
+	if (then) {
+		then.call(doSomething)
+	}
+}
 
