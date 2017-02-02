048aa2592922ba60c6523036515f4c242d0b38e0
kamushkin <kamushkin@users.noreply.github.com>
2017-02-02 18:13:42 +0100

    add more refs, comment

--- src/2583-conditions-should-not-unconditionally-evaluate-to-true-or-to-false.js
+++ src/2583-conditions-should-not-unconditionally-evaluate-to-true-or-to-false.js
@@ -3,8 +3,21 @@
 function conditionalReturn(param){
 
 	// https://sonarqube.com/issues#issues=AVn7orbraRqHwG08CTNc
+	// https://sonarqube.com/issues#issues=AVn7orcYaRqHwG08CTPt
+	// https://sonarqube.com/issues#issues=AVn7ordraRqHwG08CTWQ
 
-	// return array or null
+	/*
+
+		if (param) {
+			  return [param];
+		}
+		else {
+			  return null;
+		}
+
+
+	*/
+
 	return (param && [param]) || null;
 }
 
