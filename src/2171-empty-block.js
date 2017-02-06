e504fc26987d4ad83fc073815eb93d8686149f30
kamushkin <kamushkin@users.noreply.github.com>
2017-02-06 13:39:46 +0100

    testing EmptyBlock rule

---
+++ src/2171-empty-block.js
@@ -0,0 +1,36 @@
+
+
+function emptyBlock(param){
+
+	if (param){
+
+	}
+
+	if (param){
+		// empty
+	}
+
+	try {
+		var x = param.x;
+	}
+	catch(err){
+
+	}
+
+
+	try {
+		var y = param.y;
+	}
+	catch(err){
+		// empty
+	}
+
+}
+
+function empty(){
+
+}
+
+function empty2(){
+	// empty
+}
