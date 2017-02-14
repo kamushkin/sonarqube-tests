37fd5bd2ae7fef7048a63e11444880043582a414
kamushkin <kamushkin@users.noreply.github.com>
2017-02-14 17:35:39 +0100

    name collision

---
+++ src/name-collision.js
@@ -0,0 +1,19 @@
+
+
+function undeclaredLoopVar(){
+
+	var i, x = 0;
+
+	function doSomething(param){
+
+		// forgot to declare i
+		for(i=0; i<10; i++){
+			x += param;
+		}
+	}
+
+	for(i=0; i<42; i++){
+		doSomethng(i);
+	}
+
+}
