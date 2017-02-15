1ead5b382e221dd083e18c573c955489fcd3fc3f
kamushkin <kamushkin@users.noreply.github.com>
2017-02-15 16:29:56 +0100

    return variable

---
+++ src/return-variable.js
@@ -0,0 +1,31 @@
+
+
+function returnedVariable(){
+
+	var service = {
+
+		init: function(){
+			// init
+		},
+
+		doSomething: function(p){
+
+			service.init();
+
+			// do smth
+		}
+	};
+
+	return service;
+}
+
+
+
+function recursiveFunc(){
+
+	var fn = function(p){
+		return p ? fn(p-1) : 0;
+	};
+
+	return fn;
+}
