cae9569208f2a576ae6379541a8a433d8d6d21af
kamushkin <kamushkin@users.noreply.github.com>
2017-07-03 11:38:35 +0200

    NaN in condition eval

---
+++ src/condition-nan.js
@@ -0,0 +1,24 @@
+
+
+function compare1(a, b){
+	return a < b ? -1 : a > b ? 1 : a == b ? 0 : NaN;
+}
+
+
+function compare2(a, b){
+
+	if (a < b){
+		return -1;
+	}
+	else if (a > b){
+		return 1;
+	}
+	else if (a == b){
+		return 0;
+	}
+	else {
+		return NaN;
+	}
+}
+
+
